6a1f5099d525a26409245dedfafc1f5cac626da5
kawan idrees <kawanedres211@gmail.com>
2022-10-15 17:30:33 +0300

    test week 2

--- ACPProject.java
+++ ACPProject.java
@@ -2,5 +2,7 @@ public class ACPProject {
 
     public static void main(String[] args) {
         System.out.println("Test By Naly!!!");
+        System.out.println("hello there");
+        
     }
 }
