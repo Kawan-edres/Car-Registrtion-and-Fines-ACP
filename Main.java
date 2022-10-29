bdaa655a745da8668566f3916ea8e7f1b1b43740
Kawan Idrees <kawanedres211@gmail.com>
2022-10-30 00:18:03 +0300

    Update Main.java
    Set and HashSet is added to unique data opeartion

--- Main.java
+++ Main.java
@@ -1,200 +1,177 @@
 
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.*;
+    import java.util.*;
+    import java.io.*;
 
 
-public class Main {
+    public class Main {
 
-    static Scanner scan= new Scanner(System.in);
-    static char c;
-    // global List of fines class
-    static List<Fines> fines=new ArrayList<Fines>() ;
-    
-// global List of Car Registrtin classes 
-    static List<Truck> t = new ArrayList<Truck>();
-    static List<TransportCar> trans = new ArrayList<TransportCar>();
-    static List<PrivateCar> car = new ArrayList<PrivateCar>();
+        static Scanner scan= new Scanner(System.in);
+        static char c;
+        // global List of fines class
+        static Set<Fines> fines=new HashSet<>() ;
 
-    public static void menu(){
+    // global List of Car Registrtin classes
+        static Set<Truck> t = new HashSet<Truck>();
+        static Set<TransportCar> trans = new HashSet<TransportCar>();
+        static Set<PrivateCar> car = new HashSet<PrivateCar>();
 
-        do {
-            System.out.println("welcome to our system choose what you want :");
-            System.out.println("1)Register a Car");
-            System.out.println("2)Add Fines");
-            System.out.println("3)display Registered Cars");
-            System.out.println("4)display Registered Fines");
-            System.out.println("5)Exit");
+        public static void menu(){
+
+            do {
+                System.out.println("welcome to our system choose what you want :");
+                System.out.println("1)Register a Car");
+                System.out.println("2)Add Fines");
+                System.out.println("3)display Registered Truck");
+                System.out.println("4)display Registered Transport Cars");
+                System.out.println("5)display Registered Private Cars");
+                System.out.println("6)display Registered Fines");
+                System.out.println("7)Exit");
 
 
 
-            c = scan.next().charAt(0);
-            switch (c) {
-                case '1':
-                     System.out.println("Register New Car");
-                    addVhicle();
+                c = scan.next().charAt(0);
+                switch (c) {
+                    case '1':
+                         System.out.println("Register New Car");
+                        addVhicle();
+                        break;
+                    case '2':
+                        System.out.println("Add New fines");
+                        addFines();
+                        break;
+                    case '3':
+                        System.out.println("Display Trucks");
+                        displayTruck();
+                        break;
+                    case '4':
+                        System.out.println("Display Transport Cars");
+                        displayTransport();
+
+                        break;
+                    case '5':
+                        System.out.println("Display Registerd Car Fineses");
+                        displayPrivateCar();
+                        break;
+                    case '6':
+                    System.out.println("Display Registerd Car Fineses");
+                    dsiplayRegisteredFines();
                     break;
-                case '2':
-                    System.out.println("Add New fines");
-                    addFines();
+
+                    case '7':
+                    System.out.println("Exit");
                     break;
-                case '3':
-                    System.out.println("display Registered Cars");
-                    System.out.print("Which type of cars you want to display ?");
-                    System.out.println("\n1-Truck \n2-Transport Cars\n3-Private Cars");
-                    c=scan.next().charAt(0);
-                    displayRegisteredVhicles(c);
-                    
+
+                    default:
+                        System.out.println("you chose invalid choice!");
+                        menu();
+                        break;
+                }
+            } while (c != '7');
 
 
-                    break;
-                case '4':
-                System.out.println("Display Registerd Car Fineses");
-                
-                dsiplayRegisteredFines();
+        }
 
-                case '5':
-                System.out.println("Exit");
-                break;
+        public static void main(String[] args) {
 
-                default:
-                    System.out.println("you chose invalid choice!");
-                    menu();
-                    break;
+    //        Adminmenu();
+            menu();
+
+
+
+
+    }
+
+
+    // adding vheiclse
+    public static void addVhicle() {
+
+        System.out.print("enter vehicleType: truck || transport || private ");
+        String type = scan.next();
+        boolean check=true;
+        while (check){
+
+
+            if(!(type.equals("truck") || type.equals("transport") || type.equals("private"))){
+                System.out.println("the type of car must be truck or transport or car");
+                type=scan.next();
             }
-        } while (c != '5');
+            else {
+                check=false;
 
-    
+            }
+
+        }
+        System.out.print("enter vhicle model: ");
+        String model = scan.next();
+        System.out.print("enter vhicle Year: ");
+        Integer year = scan.nextInt();
+        System.out.print("enter annual Number:  ");
+        String annualNo = scan.next();
+        System.out.print("enter vhicle color: ");
+        String color = scan.next();
+        System.out.print("enter owner Name: ");
+        String owner = scan.next();
+        System.out.print("enter plate Number: ");
+        Integer plateNo = scan.nextInt();
+
+
+
+        System.out.print("enter vin number: ");
+        String Vin = scan.next();
+
+
+    //        checking its type
+
+        if (type.equals("truck") ) {
+
+            t.add(new Truck(1, model, year, annualNo, color, owner, plateNo, 200.0d, Vin));
+            addnewTruckToFile();
+
+        } else if (type.equals("transport")) {
+            trans.add(new TransportCar(1, model, year, annualNo, color, owner, plateNo, Vin));
+            addnewTransportCarToFile();
+        } else if(type.equals("private")) {
+
+            car.add(new PrivateCar(1, model, year, annualNo, color, owner, plateNo, Vin));
+            addNewPrivateCarToFile();
+
+
+        }
+
+    //            v.add(new Vehicle(model, year, annualNo, color, owner, plateNo, type, Vin));
+
     }
 
-    public static void main(String[] args) {
 
-    //    Adminmenu();
-        menu();
+    static void addFines(){
+        System.out.print("Enter Car Type: ");
+        String type=scan.next();
+        System.out.print("Enter CarPlate Number: ");
+        Integer plateNumber=scan.nextInt();
+        System.out.print("Enter Annual Number :");
+        String annualNo=scan.next();
+        System.out.print("Enter Fines History Date :");
+        String finesHistoryDate = scan.next();
+        System.out.print("Enter typeOf Traffic Violation :");
+        String typeOfTrafficViolation =scan.next();
+        System.out.print("Amount of Traffic Violation :");
+        Double amountOfTrafficViolationMoney =scan.nextDouble();
+        System.out.print("total Number of Traffic Violation :");
+        Integer totalViolation=scan.nextInt();
+        System.out.print("Total Violation money :");
+        Double totalMoney=scan.nextDouble();
+
+        fines.add(new Fines(type,plateNumber,annualNo,finesHistoryDate,typeOfTrafficViolation, amountOfTrafficViolationMoney,totalViolation,totalMoney));
+
         addFinesToFile();
-  
-}
-
-
-// adding vheiclse
-public static void addVhicle() {
-
-    System.out.print("enter vehicleType: truck || transport || car ");
-    String type = scan.next();
-    boolean check=true;
-    while (check){
-
-        if(!(type.equals("truck") || type.equals("transport") || type.equals("car"))){
-            System.out.println("the type of car must be truck or transport or car");
-            type=scan.next();
-        }
-        else {
-            check=false;
-
-        }
-
-    }
-    System.out.print("enter vhicle model: ");
-    String model = scan.next();
-    System.out.print("enter vhicle Year: ");
-    Integer year = scan.nextInt();
-    System.out.print("enter annual Number:  ");
-    String annualNo = scan.next();
-    System.out.print("enter vhicle color: ");
-    String color = scan.next();
-    System.out.print("enter owner Name: ");
-    String owner = scan.next();
-    System.out.print("enter plate Number: ");
-    Integer plateNo = scan.nextInt();
-  
-
-
-    System.out.println("enter vin number: ");
-    String Vin = scan.next();
-
-
-//        checking its type
-
-    if (type.equals("truck") ) {
-
-        t.add(new Truck(1, model, year, annualNo, color, owner, plateNo, 200.0d, Vin));
-
-    } else if (type.equals("transport")) {
-        trans.add(new TransportCar(1, model, year, annualNo, color, owner, plateNo, Vin));
-
-    } else if(type.equals("car")) {
-
-        car.add(new PrivateCar(1, model, year, annualNo, color, owner, plateNo, Vin));
-
 
     }
 
-//            v.add(new Vehicle(model, year, annualNo, color, owner, plateNo, type, Vin));
+    static void addFinesToFile(){
 
-}
-
-
-static void addFines(){
-    System.out.print("Enter Car Type: ");
-    String type=scan.next();
-    System.out.print("Enter CarPlate Number: ");
-    Integer plateNumber=scan.nextInt();
-    System.out.print("Enter Annual Number :");
-    String annualNo=scan.next();
-    System.out.print("Enter Fines History Date :");
-    String finesHistoryDate = scan.next();
-    System.out.print("Enter typeOf Traffic Violation :");
-    String typeOfTrafficViolation =scan.next();
-    System.out.print("Amount of Traffic Violation :");
-    Double amountOfTrafficViolationMoney =scan.nextDouble();
-    System.out.print("total Number of Traffic Violation :");
-    Integer totalViolation=scan.nextInt();
-    System.out.print("Total Violation money :");
-    Double totalMoney=scan.nextDouble();
-
-    fines.add(new Fines(type,plateNumber,annualNo,finesHistoryDate,typeOfTrafficViolation, amountOfTrafficViolationMoney,totalViolation,totalMoney));
-
-    addFinesToFile();
-
-}
-
-static void addFinesToFile(){
-
-    try{
-        ObjectOutputStream outData= new ObjectOutputStream(new FileOutputStream("FinesData.txt"));
-        outData.writeObject(fines);
-        outData.flush();
-        outData.close();
-        System.out.println("success");
-
-    }catch(IOException e){
-        e.printStackTrace();
-
-    }
-
-
-}
-
-    static void addnewTruckToFile(){
-    try{
-        ObjectOutputStream outData= new ObjectOutputStream(new FileOutputStream("truck.txt"));
-        outData.writeObject(t);
-        outData.flush();
-        outData.close();
-        System.out.println("success");
-
-    }catch(IOException e){
-        e.printStackTrace();
-
-    }
-
-
-}
-    static void addNewPrivateCarToFile(){
         try{
-            ObjectOutputStream outData= new ObjectOutputStream(new FileOutputStream("privatecar.txt"));
-            outData.writeObject(car);
+            ObjectOutputStream outData= new ObjectOutputStream(new FileOutputStream("FinesData.txt"));
+            outData.writeObject(fines);
             outData.flush();
             outData.close();
             System.out.println("success");
@@ -206,143 +183,175 @@ static void addFinesToFile(){
 
 
     }
-static  void addnewTransportCarToFile(){
-    try{
-        ObjectOutputStream outData= new ObjectOutputStream(new FileOutputStream("transport.txt"));
-        outData.writeObject(trans);
-        outData.flush();
-        outData.close();
-        System.out.println("success");
 
-    }catch(IOException e){
-        e.printStackTrace();
+    static void addnewTruckToFile(){
+        try{
+            ObjectOutputStream outData= new ObjectOutputStream(new FileOutputStream("truck.txt"));
+            outData.writeObject(t);
+            outData.flush();
+            outData.close();
+            System.out.println("success");
 
-    }
+        }catch(IOException e){
+            e.printStackTrace();
 
-}
-static void displayTruck(){
-
-
-
-    try{
-
-        ObjectInputStream in=new ObjectInputStream(new FileInputStream("truck.txt"));
-
-
-
-
-        Optional<HashSet<Truck>> checkNull = Optional.ofNullable((HashSet<Truck>)  in.readObject());
-
-//        System.out.println(checkNull);
-        if(checkNull.isEmpty()){
-            System.out.println("file is empty ");
-        }
-        else{
-            checkNull.stream().
-                    forEach( System.out::println);
         }
 
 
-        in.close();
+    }
+        static void addNewPrivateCarToFile(){
+            try{
+                ObjectOutputStream outData= new ObjectOutputStream(new FileOutputStream("privatecar.txt"));
+                outData.writeObject(car);
+                outData.flush();
+                outData.close();
+                System.out.println("success");
+
+            }catch(IOException e){
+                e.printStackTrace();
+
+            }
 
 
-    }catch (IOException e) {
-        e.printStackTrace();
-    } catch (ClassNotFoundException e) {
-        throw new RuntimeException(e);
-    }catch (Exception e){
-        e.printStackTrace();
+        }
+
+    static  void addnewTransportCarToFile(){
+        try{
+            ObjectOutputStream outData= new ObjectOutputStream(new FileOutputStream("transport.txt"));
+            outData.writeObject(trans);
+            outData.flush();
+            outData.close();
+            System.out.println("success");
+
+        }catch(IOException e){
+            e.printStackTrace();
+
+        }
 
     }
+    static void displayTruck(){
 
 
-}
-
-static void displayTransport(){
 
         try{
 
-            ObjectInputStream in=new ObjectInputStream(new FileInputStream("transport.txt"));
+            ObjectInputStream in=new ObjectInputStream(new FileInputStream("truck.txt"));
 
 
-            Optional<HashSet<TransportCar>> checkNull = Optional.ofNullable((HashSet<TransportCar>)  in.readObject());
+
+
+            Optional<HashSet<Truck>> checkNull = Optional.ofNullable((HashSet<Truck>)  in.readObject());
+
+    //        System.out.println(checkNull);
+            if(checkNull.isEmpty()){
+                System.out.println("file is empty ");
+            }
+            else{
+                checkNull.stream().
+                        forEach( System.out::println);
+            }
 
 
             in.close();
-            checkNull.stream().
-                    forEach( System.out::println);
-            System.out.println("sucess");
+
+
         }catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-
-    static void displayPrivateCar(){
-
-        try{
-
-            ObjectInputStream in=new ObjectInputStream(new FileInputStream("privatecar.txt"));
-
-
-
-            Optional<HashSet<PrivateCar>> checkNull = Optional.ofNullable((HashSet<PrivateCar>) in.readObject());
-
-            in.close();
-            checkNull.stream().
-                    forEach( System.out::println);
-            System.out.println("sucess");
-        }catch (IOException e) {
+        }catch (Exception e){
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-static void dsiplayRegisteredFines(){
-
-        try{
-
-            ObjectInputStream in=new ObjectInputStream(new FileInputStream("FinesData.txt"));
-
-            Optional<HashSet<Fines>> checkNull = Optional.ofNullable((HashSet<Fines>) in.readObject());
-            in.close();
-            checkNull.
-                    stream().
-                   forEach(p-> System.out.println(p));;
-                   System.out.println("sucess");
-        }catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
         }
 
 
     }
 
-public static void Adminmenu(){
+        static void displayTransport(){
+
+            try{
+
+                ObjectInputStream in=new ObjectInputStream(new FileInputStream("transport.txt"));
 
 
-    System.out.println("Welcome to the car registration System and fines");
-    System.out.println("enter 1 if you want to login : ->");
-    System.out.println("1-Login ");
-    int option=scan.nextInt();
+                Optional<HashSet<TransportCar>> checkNull = Optional.ofNullable((HashSet<TransportCar>)  in.readObject());
 
-    switch (option){
-        case 1: AdminAccount.Login();
 
-        break;
-        default: break;
+                in.close();
+                checkNull.stream().
+                        forEach( System.out::println);
+                System.out.println("sucess");
+            }catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
+
+        static void displayPrivateCar(){
+
+            try{
+
+                ObjectInputStream in=new ObjectInputStream(new FileInputStream("privatecar.txt"));
+
+
+
+                Optional<HashSet<PrivateCar>> checkNull = Optional.ofNullable((HashSet<PrivateCar>) in.readObject());
+
+                in.close();
+                checkNull.stream().
+                        forEach( System.out::println);
+                System.out.println("sucess");
+            }catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        static void dsiplayRegisteredFines(){
+
+            try{
+
+                ObjectInputStream in=new ObjectInputStream(new FileInputStream("FinesData.txt"));
+
+                Optional<HashSet<Fines>> checkNull = Optional.ofNullable((HashSet<Fines>) in.readObject());
+                in.close();
+                checkNull.
+                        stream().
+                       forEach(p-> System.out.println(p));;
+                       System.out.println("sucess");
+            }catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
+    public static void Adminmenu(){
+
+
+        System.out.println("Welcome to the car registration System and fines");
+        System.out.println("enter 1 if you want to login : ->");
+        System.out.println("1-Login ");
+        int option=scan.nextInt();
+
+        switch (option){
+            case 1: AdminAccount.Login();
+
+            break;
+            default: break;
+
+
+        }
 
     }
 
-}
 
 
 
-
-}
+    }
 
