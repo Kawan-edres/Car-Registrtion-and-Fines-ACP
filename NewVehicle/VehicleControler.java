f80b476ad5cbd614f7c22115a5cac83985ddc166
Bahez <95364328+Bahezz@users.noreply.github.com>
2022-11-13 02:07:28 +0300

    Update VehicleControler.java
    adding VehicleControler class

--- NewVehicle/VehicleControler.java
+++ NewVehicle/VehicleControler.java
@@ -1,1 +1,105 @@
+package Vehicle;
+
+import java.io.Serializable;
+
+public class VehicleControler implements Serializable {
+    VehicleModule module;
+    VehicleView view;
+
+    public VehicleControler(VehicleModule module, VehicleView view) {
+       
+        this.module = module;
+        this.view = view;
+    }
+
+
+
+
+    public void addVhicle() {
+                                
+        this.module.addVhicle(   this.view.addVhicle()   );
+
+    }
+
+
+
+
+    public void displayVehicle(String Type) {
+
+        
+
+        if (Type.equals("truck")){
+
+
+           this.module.readTrukFromFile();
+
+             this.view.displayTruck(module.getT());
+
+        }
+           
+        else if (Type.equals("transport")){
+            this.module.readTransportFromFile();
+             this.view.displayTransport(module.getTrans());
+        }
+           
+
+        else {
+            this.module.readPrivateCarFromFile();
+             this.view.displayPrivateCar(module.getCar());
+        }
+       
+           
+
+    }
+
+    public void displayAll(){
+        // truck 
+        this.module.readTrukFromFile();
+
+             this.view.displayTruck(module.getT());
+        // transport
+        this.module.readTransportFromFile();
+             this.view.displayTransport(module.getTrans());
+        // private
+        this.module.readPrivateCarFromFile();
+             this.view.displayPrivateCar(module.getCar());
+        
+    }
+
+    public void serchForCar(String annual, String Type ){
+
+        
+        if (Type.equals("truck")){
+
+
+            this.module.readTrukFromFile();
+            this.module.getT().stream().filter(p->p.getAnnualNumber().equals(annual)).forEach(System.out::println);
+           
+         }
+            
+         else if (Type.equals("transport")){
+             this.module.readTransportFromFile();
+              this.module.getTrans().stream().filter(p->p.getAnnualNumber().equals(annual)).forEach(System.out::println);
+         }
+            
+ 
+         else if(Type.equals("private")) {
+             this.module.readPrivateCarFromFile();
+              this.module.getCar().stream().filter(p->p.getAnnualNumber().equals(annual)).forEach(System.out::println);
+         }
+        
+        
+
+
+    }
+
+     
+
+   
+
+
+
+
+
+}
 
