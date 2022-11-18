47ab71cb0e6df6825b1ad60ecb6ea51e40017dec
Bahez <95364328+Bahezz@users.noreply.github.com>
2022-11-19 00:51:28 +0300

    Update RenewAnnualModel.java

--- Models/RenewAnnualModel.java
+++ Models/RenewAnnualModel.java
@@ -1,1 +1,18 @@
+package Models;
 
+import java.io.Serializable;
+import java.util.Objects;
+
+public class RenewAnnualModel implements Serializable {
+    private Integer vehiclePlateNumber;
+    private String ownerFullName;
+    private String ownerAddress;
+    private String carModel;
+    private String carType;
+    private String Vin;
+    private String carColor;
+    private String  startDate;
+    private String endDate;
+    private String vheicleSafetyEndDate;
+    private String environmentFinesEndDate;
+    private Integer vheicleChainNumber;//sequnece Number
