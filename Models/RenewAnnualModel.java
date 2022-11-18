8eae86d33433393cc556af56bc746bf0692fbc82
Bahez <95364328+Bahezz@users.noreply.github.com>
2022-11-19 00:52:18 +0300

    Update RenewAnnualModel.java
    constructer added

--- Models/RenewAnnualModel.java
+++ Models/RenewAnnualModel.java
@@ -16,3 +16,20 @@ public class RenewAnnualModel implements Serializable {
     private String vheicleSafetyEndDate;
     private String environmentFinesEndDate;
     private Integer vheicleChainNumber;//sequnece Number
+
+    public RenewAnnualModel(Integer vehicleNumber, String fullName, String address, String carModel,
+                            String carType, String carColor, String startDate, String endDate, Integer vheicleChainNumber, String carSafetyEndDate,
+                            String environmentFinesEndDate, String vin) {
+        this.vehiclePlateNumber = vehicleNumber;
+        this.ownerFullName = fullName;
+        this.ownerAddress = address;
+        this.carModel = carModel;
+        this.carType=carType;
+        this.carColor = carColor;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.vheicleChainNumber = vheicleChainNumber;
+        this.vheicleSafetyEndDate = carSafetyEndDate;
+        this.environmentFinesEndDate = environmentFinesEndDate;
+        this.Vin=vin;
+    }
