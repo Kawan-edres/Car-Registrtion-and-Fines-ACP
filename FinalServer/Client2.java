cfcf696f5e9d0193e93ea4bfb7c74584a01e69f2
Naly Moslih <95342461+Nalymoslih@users.noreply.github.com>
2022-11-20 02:18:26 +0300

    Update Client2.java

--- FinalServer/Client2.java
+++ FinalServer/Client2.java
@@ -1,1 +1,15 @@
+package FinalServer;
 
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public static void main(String[] args) throws IOException {
+        Socket echoSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
