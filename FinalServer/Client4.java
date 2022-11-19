b9017b48210329a26209feeda83e135d7be0bc73
Elaf.R. Ibrahim <92952624+ElafRadha@users.noreply.github.com>
2022-11-20 02:27:41 +0300

    Update Client4.java
    client user additioan parale straming is added

--- FinalServer/Client4.java
+++ FinalServer/Client4.java
@@ -33,3 +33,18 @@ public class Client2 {
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));//scanner
         String userInput;
         System.out.println("connected");
+      while ((userInput = in.readLine()) !=   null) {
+
+            boolean isServerAskForInput = userInput.equals("k");
+            if (isServerAskForInput) {    // 1 if send then is ask for input from client
+                System.out.print("clint : ");
+                out.println(stdIn.readLine());
+            }
+            if(!isServerAskForInput)
+                System.out.println("-" + userInput);
+
+            if(userInput.equals("exit")){
+                System.exit(0);
+                break;
+            }
+        }
