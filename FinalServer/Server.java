b23ccc42a495c1bdab1f839550484bf7cc6310d8
Kawan Idrees <kawanedres211@gmail.com>
2022-11-20 02:31:20 +0300

    Update Server.java
    Multi threaded echoer is added to our system hope fully after along of time and failnes and trying again i could magnage it

--- FinalServer/Server.java
+++ FinalServer/Server.java
@@ -1,56 +1,53 @@
 package FinalServer;
-
-
-
+import Views.GeneralView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.ParseException;
 
-import Views.GeneralView;
 
-public class Server {
+public class Server extends Thread {
+    private Socket socket;
+    BufferedReader input=null;
+    PrintWriter output=null;
 
-    public void startConnection(){
-
-
-
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-
-        try {
-            serverSocket = new ServerSocket(6000);
-            System.out.println("waiting for client connection");
-            clientSocket = serverSocket.accept();
-            System.out.println("Connected to a client");
-
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-
-            GeneralView.menu(out,in);
-
-
-
-
-            
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-
-        }
-        catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-
-
-
-
-
+    public Server(Socket socket) {
+        this.socket = socket;
     }
 
+    @Override
+    public void run() {
+        try {
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            try{
+                Thread.sleep(3000);
+            }catch (InterruptedException e){
+                System.out.println("thread inturupted "+e.getMessage());
+            }
+                GeneralView.menu(output,input);
+
+        } catch(IOException e) {
+            System.out.println("Oops: " + e.getMessage());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                output.close();
+                input.close();
+                socket.close();
+            } catch(IOException e) {
+                
+            }
+        }
+
+    }
 }
+
+
+
+
