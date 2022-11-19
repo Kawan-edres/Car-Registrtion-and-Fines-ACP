d3ca4fa92695032bf6fdab8490ed7723e60b3c78
Goran <93624421+goransafeen@users.noreply.github.com>
2022-11-20 02:55:31 +0300

    Update RenewAnnualView.java
    formatted

--- Views/RenewAnnualView.java
+++ Views/RenewAnnualView.java
@@ -1,8 +1,6 @@
 package Views;
 
-
 import Controllers.RenewController;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,91 +9,94 @@ import java.util.Scanner;
 
 public class RenewAnnualView {
 
+  static Scanner scan = new Scanner(System.in);
 
-    static Scanner scan= new Scanner(System.in);
-    static void menu(PrintWriter out, BufferedReader in) throws ParseException, IOException {
+  static void menu(PrintWriter out, BufferedReader in)
+    throws ParseException, IOException {
+    String c;
+    do {
+      out.println("Welcome to the  Car Fines System ");
+      out.println("------------------------------------");
+      out.println("Choose What do you want :");
+      out.println("1-Add Annual ");
+      out.println("2-Delete Annual");
+      out.println("3-Display All Annual ");
+      out.println("4-Search for spesfic Car");
+      out.println("5-Exit");
+      out.println("k");
+      c = in.readLine();
 
-        String c;
-        do {
-            out.println("Welcome to the  Car Fines System ");
-            out.println("------------------------------------");
-            out.println("Choose What do you want :");
-            out.println("1-Add Annual ");
-            out.println("2-Delete Annual");
-            out.println("3-Display All Annual ");
-            out.println("4-Search for spesfic Car");
-            out.println("5-Exit");
-            out.println("k");
-            c = in.readLine();
-
-
-            switch (c) {
-                case "1":
-                    RenewController.addRenewAnnualData(out,in);
-                    break;
-                case "2":
-
-                    out.println("Enter car type truck or transport or private :");
-                    out.println("k");
-                    String typeTodelete= in.readLine();
-                    boolean check = true;
-                    while (check) {
-                        if (!(typeTodelete.equals("truck") || typeTodelete.equals("private") || typeTodelete.equals("transport"))) {
-                            out.println("the type should be truck or private or transport");
-                            out.println("k");
-                            typeTodelete= in.readLine();
-                        } else {
-                            check = false;
-                            break;
-                        }
-
-                    }
-                    out.println("Deleting a Annuals ");
-                    out.println("Enter Plate Number of Vehechle: ");
-                    out.println("k");
-                    int plateNumberDelete = Integer.parseInt(in.readLine());
-                    RenewController.delete(plateNumberDelete,typeTodelete);
-                    out.println("Deleted");
-                    break;
-                case "3":
-                    out.println("Display All Annuals");
-                    RenewController.print(out);
-                    break;
-
-
-                case "4":
-                    out.println("Search For Spesfic Car Fines");
-                    out.println("Enter Plate Number of Vehechle: ");
-                    out.println("k");
-
-                    int plateNumberForSearch=Integer.parseInt(in.readLine());
-                    out.println("Enter car type truck or transport or private :");
-                    out.println("k");
-                    String typeToSearch= in.readLine();
-                    boolean check2 = true;
-                    while (check2) {
-                        if (!(typeToSearch.equals("truck") || typeToSearch.equals("private") || typeToSearch.equals("transport"))) {
-                            out.println("the type should be truck or private or transport");
-                            out.println("k");
-                            typeToSearch= in.readLine();
-                        } else {
-                            check2 = false;
-                            break;
-                        }
-
-                    }
-
-                    RenewController.search(out,plateNumberForSearch,typeToSearch);
-                    break;
-                case "5":
-                    out.println("exit");
-                default:
-                   out.println("Choose on of the above options to start !");
-                    break;
-
+      switch (c) {
+        case "1":
+          RenewController.addRenewAnnualData(out, in);
+          break;
+        case "2":
+          out.println("Enter car type truck or transport or private :");
+          out.println("k");
+          String typeTodelete = in.readLine();
+          boolean check = true;
+          while (check) {
+            if (
+              !(
+                typeTodelete.equals("truck") ||
+                typeTodelete.equals("private") ||
+                typeTodelete.equals("transport")
+              )
+            ) {
+              out.println("the type should be truck or private or transport");
+              out.println("k");
+              typeTodelete = in.readLine();
+            } else {
+              check = false;
+              break;
             }
+          }
+          out.println("Deleting a Annuals ");
+          out.println("Enter Plate Number of Vehechle: ");
+          out.println("k");
+          int plateNumberDelete = Integer.parseInt(in.readLine());
+          RenewController.delete(plateNumberDelete, typeTodelete);
+          out.println("Deleted");
+          break;
+        case "3":
+          out.println("Display All Annuals");
+          RenewController.print(out);
+          break;
+        case "4":
+          out.println("Search For Spesfic Car Fines");
+          out.println("Enter Plate Number of Vehechle: ");
+          out.println("k");
 
-        } while (c != "5");
+          int plateNumberForSearch = Integer.parseInt(in.readLine());
+          out.println("Enter car type truck or transport or private :");
+          out.println("k");
+          String typeToSearch = in.readLine();
+          boolean check2 = true;
+          while (check2) {
+            if (
+              !(
+                typeToSearch.equals("truck") ||
+                typeToSearch.equals("private") ||
+                typeToSearch.equals("transport")
+              )
+            ) {
+              out.println("the type should be truck or private or transport");
+              out.println("k");
+              typeToSearch = in.readLine();
+            } else {
+              check2 = false;
+              break;
+            }
+          }
 
-    }
+          RenewController.search(out, plateNumberForSearch, typeToSearch);
+          break;
+        case "5":
+          out.println("exit");
+        default:
+          out.println("Choose on of the above options to start !");
+          break;
+      }
+    } while (c != "5");
+  }
 }
