7066e410d946ec6fa3cf832a54c180473823b7b7
Goran <93624421+goransafeen@users.noreply.github.com>
2022-11-20 02:52:14 +0300

    Update VehicleModule.java
    formatted

--- Models/VehicleModule.java
+++ Models/VehicleModule.java
@@ -1,6 +1,5 @@
 package Models;
 
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,149 +9,122 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-
-
 public class VehicleModule implements Serializable {
-  private  static Set<Truck> t = new HashSet<>();
-  private  static Set<TransportCar> trans = new HashSet<TransportCar>();
-  private  static Set<PrivateCar> car = new HashSet<PrivateCar>();
 
-    public void addVhicle(Vehicle v){// v= ragrover 
+  private static Set<Truck> t = new HashSet<>();
+  private static Set<TransportCar> trans = new HashSet<TransportCar>();
+  private static Set<PrivateCar> car = new HashSet<PrivateCar>();
 
-        if (v.getVehicleType().equals("truck") ) {
+  public void addVhicle(Vehicle v) { // v= ragrover
+    if (v.getVehicleType().equals("truck")) {
+      readTrukFromFile();
 
-            readTrukFromFile();
+      t.add((Truck) v);
+      addnewTruckToFile();
+    } else if (v.getVehicleType().equals("transport")) {
+      readTransportFromFile();
+      trans.add((TransportCar) v);
 
-       t.add((Truck)v);
-            addnewTruckToFile();
+      aaddnewTransportCarToFile();
+    } else if (v.getVehicleType().equals("private")) {
+      readPrivateCarFromFile();
+      car.add((PrivateCar) v);
 
-        } else if (v.getVehicleType().equals("transport")) {
-
-            readTransportFromFile();
-           trans.add((TransportCar)v);
-
-            aaddnewTransportCarToFile();
-          
-        } else if(v.getVehicleType().equals("private")) {
-            readPrivateCarFromFile();
-            car.add((PrivateCar)v);
-          
-            addnewPrivateCarToFile();
-
-        }
-
+      addnewPrivateCarToFile();
     }
+  }
 
-    
-
-    // Truck
-    public void addnewTruckToFile() {
-        try {
-            ObjectOutputStream outData = new ObjectOutputStream(new FileOutputStream("Truck.txt"));
-            outData.writeObject(t);
-            outData.flush();
-            outData.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        }
-
+  // Truck
+  public void addnewTruckToFile() {
+    try {
+      ObjectOutputStream outData = new ObjectOutputStream(
+        new FileOutputStream("Truck.txt")
+      );
+      outData.writeObject(t);
+      outData.flush();
+      outData.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    //transport car
-     public void aaddnewTransportCarToFile() {
-        try {
-            ObjectOutputStream outData = new ObjectOutputStream(new FileOutputStream("TransportCars.txt"));
-            outData.writeObject(trans);
-            outData.flush();
-            outData.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        }
-
+  //transport car
+  public void aaddnewTransportCarToFile() {
+    try {
+      ObjectOutputStream outData = new ObjectOutputStream(
+        new FileOutputStream("TransportCars.txt")
+      );
+      outData.writeObject(trans);
+      outData.flush();
+      outData.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    //private car 
-     public void addnewPrivateCarToFile() {
-        try {
-            ObjectOutputStream outData = new ObjectOutputStream(new FileOutputStream("PrivateCars.txt"));
-            outData.writeObject(car);
-            outData.flush();
-            outData.close();
+  }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        }
-
+  //private car
+  public void addnewPrivateCarToFile() {
+    try {
+      ObjectOutputStream outData = new ObjectOutputStream(
+        new FileOutputStream("PrivateCars.txt")
+      );
+      outData.writeObject(car);
+      outData.flush();
+      outData.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-
-     public void readTrukFromFile() {
-        try {
-
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream("Truck.txt"));
-            t = (HashSet<Truck>) in.readObject();
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
+  public void readTrukFromFile() {
+    try {
+      ObjectInputStream in = new ObjectInputStream(
+        new FileInputStream("Truck.txt")
+      );
+      t = (HashSet<Truck>) in.readObject();
+      in.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-
-     public void readTransportFromFile() {
-        try {
-
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream("TransportCars.txt"));
-            trans = (HashSet<TransportCar>) in.readObject();
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
+  public void readTransportFromFile() {
+    try {
+      ObjectInputStream in = new ObjectInputStream(
+        new FileInputStream("TransportCars.txt")
+      );
+      trans = (HashSet<TransportCar>) in.readObject();
+      in.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
     }
+  }
 
+  public void readPrivateCarFromFile() {
+    try {
+      ObjectInputStream in = new ObjectInputStream(
+        new FileInputStream("PrivateCars.txt")
+      );
+      car = (HashSet<PrivateCar>) in.readObject();
+      in.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {}
+  }
 
+  public Set<Truck> getT() {
+    return t;
+  }
 
-     public void readPrivateCarFromFile() {
-        try {
+  public Set<TransportCar> getTrans() {
+    return trans;
+  }
 
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream("PrivateCars.txt"));
-           car = (HashSet<PrivateCar>) in.readObject();
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-          
-        }
-
-    }
-
-
-
-    public  Set<Truck> getT() {
-        return t;
-    }
-
-
-
-    public Set<TransportCar> getTrans() {
-        return trans;
-    }
-
-
-
-    public Set<PrivateCar> getCar() {
-        return car;
-    }
-
-
-
+  public Set<PrivateCar> getCar() {
+    return car;
+  }
 }
