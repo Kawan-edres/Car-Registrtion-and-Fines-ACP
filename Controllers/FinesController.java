088f0325246130bb6fbb92904ceda095eac1c4bd
Goran <93624421+goransafeen@users.noreply.github.com>
2022-11-20 01:05:50 +0300

    Update FinesController.java
    formated

--- Controllers/FinesController.java
+++ Controllers/FinesController.java
@@ -1,9 +1,6 @@
 package Controllers;
 
-
-
 import Models.FinesModel;
-
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -16,100 +13,119 @@ import java.util.Set;
 
 public class FinesController {
 
-    static Scanner scan = new Scanner(System.in);
+  static Scanner scan = new Scanner(System.in);
 
-    private static Set<FinesModel> fines = new HashSet<>();
+  private static Set<FinesModel> fines = new HashSet<>();
 
-
-
-
-    public static void addFinesData(PrintWriter out, BufferedReader in) throws ParseException, IOException {
-        out.println("Enter Car Type: ");
+  public static void addFinesData(PrintWriter out, BufferedReader in)
+    throws ParseException, IOException {
+    out.println("Enter Car Type: ");
+    out.println("k");
+    String type = in.readLine();
+    boolean check = true;
+    while (check) {
+      if (
+        !(
+          type.equals("truck") ||
+          type.equals("private") ||
+          type.equals("transport")
+        )
+      ) {
+        out.println("the type shuld be truck or private or transport");
         out.println("k");
-        String type = in.readLine();
-        boolean check = true;
-        while (check) {
-            if (!(type.equals("truck") || type.equals("private") || type.equals("transport"))) {
-                out.println("the type shuld be truck or private or transport");
-                out.println("k");
-                type = in.readLine();
-            } else {
-                check = false;
-                break;
-            }
-
-        }
-        out.println("Enter CarPlate Number: ");
-        out.println("k");
-        Integer plateNumber = Integer.parseInt(in.readLine());
-        out.println("Enter Annual Number :");
-        out.println("k");
-        String annualNo = in.readLine();
-        out.println("Enter typeOf Traffic Violation :");
-        out.println("k");
-        String typeOfTrafficViolation = in.readLine();
-        out.println("Amount of Traffic Violation :");
-        out.println("k");
-        Double amountOfTrafficViolationMoney = Double.valueOf(in.readLine());
-        out.println("total Number of Traffic Violation :");
-        out.println("k");
-        Integer totalViolation = Integer.parseInt(in.readLine());
-        out.println("Total Violation money :");
-        out.println("k");
-        Double totalMoney = Double.valueOf(in.readLine());
-
-        LocalDate dateObj = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        String date = dateObj.format(formatter);
-        Date date1 = new SimpleDateFormat("dd/MM/yyyy").parse(date);
-
-        readFinesFromFile();
-        fines.add(new FinesModel(type, plateNumber, annualNo, date1, typeOfTrafficViolation, amountOfTrafficViolationMoney, totalViolation, totalMoney));
-        writeFinesToFile();
+        type = in.readLine();
+      } else {
+        check = false;
+        break;
+      }
     }
+    out.println("Enter CarPlate Number: ");
+    out.println("k");
+    Integer plateNumber = Integer.parseInt(in.readLine());
+    out.println("Enter Annual Number :");
+    out.println("k");
+    String annualNo = in.readLine();
+    out.println("Enter typeOf Traffic Violation :");
+    out.println("k");
+    String typeOfTrafficViolation = in.readLine();
+    out.println("Amount of Traffic Violation :");
+    out.println("k");
+    Double amountOfTrafficViolationMoney = Double.valueOf(in.readLine());
+    out.println("total Number of Traffic Violation :");
+    out.println("k");
+    Integer totalViolation = Integer.parseInt(in.readLine());
+    out.println("Total Violation money :");
+    out.println("k");
+    Double totalMoney = Double.valueOf(in.readLine());
 
-    static void writeFinesToFile() {
-        try {
-            ObjectOutputStream outData = new ObjectOutputStream(new FileOutputStream("FinesTest.txt"));
-            outData.writeObject(fines);
-            outData.flush();
-            outData.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    LocalDate dateObj = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+    String date = dateObj.format(formatter);
+    Date date1 = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+
+    readFinesFromFile();
+    fines.add(
+      new FinesModel(
+        type,
+        plateNumber,
+        annualNo,
+        date1,
+        typeOfTrafficViolation,
+        amountOfTrafficViolationMoney,
+        totalViolation,
+        totalMoney
+      )
+    );
+    writeFinesToFile();
+  }
+
+  static void writeFinesToFile() {
+    try {
+      ObjectOutputStream outData = new ObjectOutputStream(
+        new FileOutputStream("FinesTest.txt")
+      );
+      outData.writeObject(fines);
+      outData.flush();
+      outData.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    static void readFinesFromFile() {
-        try {
-
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream("FinesTest.txt"));
-            fines = (HashSet<FinesModel>) in.readObject();
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
+  static void readFinesFromFile() {
+    try {
+      ObjectInputStream in = new ObjectInputStream(
+        new FileInputStream("FinesTest.txt")
+      );
+      fines = (HashSet<FinesModel>) in.readObject();
+      in.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public static void printFines(PrintWriter out) {
-        readFinesFromFile();
-        fines.stream().forEach(p -> out.println(p));
+  public static void printFines(PrintWriter out) {
+    readFinesFromFile();
+    fines.stream().forEach(p -> out.println(p));
+  }
 
-    }
+  public static void deleteFines(int plateNumber, String type) {
+    readFinesFromFile();
+    fines.removeIf(p ->
+      (p.getCarPlateNumber() == plateNumber && p.getCarType().equals(type))
+    );
+    writeFinesToFile();
+  }
 
-    public static void deleteFines(int plateNumber, String type) {
-        readFinesFromFile();
-        fines.removeIf(p -> (p.getCarPlateNumber() == plateNumber && p.getCarType().equals(type)));
-        writeFinesToFile();
-
-    }
-    public static  void search(PrintWriter out,int plateNumber,String type){
-        readFinesFromFile();
-        fines.stream().filter(p-> p.getCarPlateNumber()==plateNumber && p.getCarType().equals(type)).forEach(p-> out.println(p));
-
-
-    }
-
+  public static void search(PrintWriter out, int plateNumber, String type) {
+    readFinesFromFile();
+    fines
+      .stream()
+      .filter(p ->
+        p.getCarPlateNumber() == plateNumber && p.getCarType().equals(type)
+      )
+      .forEach(p -> out.println(p));
+  }
 }
