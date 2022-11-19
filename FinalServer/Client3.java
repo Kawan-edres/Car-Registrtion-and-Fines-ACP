e35b2d7e21eb0a6435d2d27cbea927aceea1e5cc
Goran <93624421+goransafeen@users.noreply.github.com>
2022-11-20 02:25:20 +0300

    Update Client3.java
    updated

--- FinalServer/Client3.java
+++ FinalServer/Client3.java
@@ -11,3 +11,13 @@ public class Client3 {
         Socket echoSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
+try {
+            echoSocket = new Socket("localhost", 6000);
+            System.out.println("connected to: "+echoSocket);
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
