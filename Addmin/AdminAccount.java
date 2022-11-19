7728468ba1dbf8e97884f28c8084bcb55b65393c
Goran <93624421+goransafeen@users.noreply.github.com>
2022-11-20 01:03:19 +0300

    Update AdminAccount.java
    formated

--- Addmin/AdminAccount.java
+++ Addmin/AdminAccount.java
@@ -1,76 +1,78 @@
 package Addmin;
-import java.util.Scanner;
+
 import java.io.*;
+import java.util.Scanner;
 
 public class AdminAccount {
 
-    static Scanner scan = new Scanner(System.in);
+  static Scanner scan = new Scanner(System.in);
 
+  //instance
+  private String emaill;
+  private String passwordd;
 
-    // instance
-    private String emaill;
-    private String passwordd;
+  public AdminAccount(String email, String password) {
+    this.emaill = email;
+    this.passwordd = password;
+  }
 
-    public AdminAccount(String email, String password) {
+  public static void Login() {
+    System.out.print("enter yout email :");
+    String emailInput = scan.next();
+    System.out.print("enter your password :");
+    String passwordInput = scan.next();
 
-        this.emaill = email;
-        this.passwordd = password;
-    }
+    String validEmail = emailInput.trim().toLowerCase();
 
-    public static void Login() {
-        System.out.print("enter yout email :");
-        String emailInput = scan.next();
-        System.out.print("enter your password :");
-        String passwordInput = scan.next();
+    try {
+      BufferedReader br = new BufferedReader(new FileReader("admin.txt"));
 
-        String validEmail = emailInput.trim().toLowerCase();
+      String line = br.readLine(); //
 
-        try{
-            BufferedReader br= new BufferedReader(new FileReader("admin.txt"));
-
-            String line = br.readLine();//
-
-            while (line !=null){
-                if(validEmail.equals(line)){
-
-                    line=br.readLine(); //12345
-                    if(passwordInput.equals(line)){
-                        break;
-
-                    }
-                }
-                    else {
-                        System.out.println("email or passwor is not correct try again");
-                        Login();
-                    }
-            }
-        br.close();
-        }catch(IOException e){
-            e.printStackTrace();
+      while (line != null) {
+        if (validEmail.equals(line)) {
+          line = br.readLine(); //12345
+          if (passwordInput.equals(line)) {
+            break;
+          }
+        } else {
+          System.out.println("email or passwor is not correct try again");
+          Login();
         }
+      }
+      br.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public String getEmail() {
-        return emaill;
-    }
+  public String getEmail() {
+    return emaill;
+  }
 
-    public void setEmail(String email) {
-        this.emaill = email;
-    }
+  public void setEmail(String email) {
+    this.emaill = email;
+  }
 
-    public String getPassword() {
-        return passwordd;
-    }
+  public String getPassword() {
+    return passwordd;
+  }
 
-    public void setPassword(String password) {
-        this.passwordd = password;
-    }
+  public void setPassword(String password) {
+    this.passwordd = password;
+  }
 
-    @Override
-    public String toString() {
-        return "AdminLogin{" +
-                "email='" + emaill + '\'' +
-                ", password='" + passwordd + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return (
+      "AdminLogin{" +
+      "email='" +
+      emaill +
+      '\'' +
+      ", password='" +
+      passwordd +
+      '\'' +
+      '}'
+    );
+  }
 }
