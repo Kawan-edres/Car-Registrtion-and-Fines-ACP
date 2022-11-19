5f3edb9ad687680fc6b45c9d0b0c65e20bf5a44d
Goran <93624421+goransafeen@users.noreply.github.com>
2022-11-20 02:54:36 +0300

    Update GeneralView.java
    formatted

--- Views/GeneralView.java
+++ Views/GeneralView.java
@@ -7,43 +7,37 @@ import java.text.ParseException;
 
 public class GeneralView {
 
-//    public static void main(String[] args) throws ParseException {
-//        menu();
-//
-//    }
+  //    public static void main(String[] args) throws ParseException {
+  //        menu();
+  //
+  //    }
 
+  public static void menu(PrintWriter out, BufferedReader in)
+    throws ParseException, IOException {
+    String c;
+    do {
+      out.println("Welcome to the  Car registertion and Fines System  ");
+      out.println("------------------------------------");
+      out.println("Choose On bellow options to Login to the system  :");
+      out.println("1-Admin Login ");
+      out.println("2-User Fines View ");
+      out.println("3-Exit");
+      out.println("k");
+      c = in.readLine();
 
-    public static void menu(PrintWriter out, BufferedReader in) throws ParseException, IOException {
-
-        String c;
-        do {
-            out.println("Welcome to the  Car registertion and Fines System  ");
-            out.println("------------------------------------");
-            out.println("Choose On bellow options to Login to the system  :");
-            out.println("1-Admin Login ");
-            out.println("2-User Fines View ");
-            out.println("3-Exit");
-            out.println("k");
-            c = in.readLine();
-
-
-            switch (c) {
-                case "1":
-                    AdminView.menu(out,in);
-                    break;
-                case "2":
-                    UserView.menu(out,in);
-                    break;
-                case "3":
-                    out.println("exit");
-                default:
-                    out.println("Choose on of the above options to start !");
-                    break;
-
-            }
-
-        } while (c != "3");
-
-
-    }
+      switch (c) {
+        case "1":
+          AdminView.menu(out, in);
+          break;
+        case "2":
+          UserView.menu(out, in);
+          break;
+        case "3":
+          out.println("exit");
+        default:
+          out.println("Choose on of the above options to start !");
+          break;
+      }
+    } while (c != "3");
+  }
 }
