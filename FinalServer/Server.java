67663d107edf90c2ab63a20070ed8b332772cf57
Goran <93624421+goransafeen@users.noreply.github.com>
2022-11-20 02:49:11 +0300

    Update Server.java
    formatted

--- FinalServer/Server.java
+++ FinalServer/Server.java
@@ -1,6 +1,6 @@
 package FinalServer;
-import Views.GeneralView;
 
+import Views.GeneralView;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,46 +8,39 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.text.ParseException;
 
-
 public class Server extends Thread {
-    private Socket socket=null;
-    BufferedReader input=null;
-    PrintWriter output=null;
 
-    public Server(Socket socket) {
-        this.socket = socket;
+  private Socket socket = null;
+  BufferedReader input = null;
+  PrintWriter output = null;
+
+  public Server(Socket socket) {
+    this.socket = socket;
+  }
+
+  @Override
+  public void run() {
+    try {
+      BufferedReader input = new BufferedReader(
+        new InputStreamReader(socket.getInputStream())
+      );
+      PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+      try {
+        Thread.sleep(3000);
+      } catch (InterruptedException e) {
+        System.out.println("thread inturupted " + e.getMessage());
+      }
+      GeneralView.menu(output, input);
+    } catch (IOException e) {
+      System.out.println("Oops: " + e.getMessage());
+    } catch (ParseException e) {
+      throw new RuntimeException(e);
+    } finally {
+      try {
+        output.close();
+        input.close();
+        socket.close();
+      } catch (IOException e) {}
     }
-
-    @Override
-    public void run() {
-        try {
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-            try{
-                Thread.sleep(3000);
-            }catch (InterruptedException e){
-                System.out.println("thread inturupted "+e.getMessage());
-            }
-                GeneralView.menu(output,input);
-
-        } catch(IOException e) {
-            System.out.println("Oops: " + e.getMessage());
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                output.close();
-                input.close();
-                socket.close();
-            } catch(IOException e) {
-                
-            }
-        }
-
-    }
+  }
 }
-
-
-
-
