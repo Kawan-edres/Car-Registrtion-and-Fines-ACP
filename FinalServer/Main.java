2ed4c1ea2fe21e9d5cba36fc9e2bb69e89bdc2a9
Goran <93624421+goransafeen@users.noreply.github.com>
2022-11-20 02:48:28 +0300

    Update Main.java
    formatted

--- FinalServer/Main.java
+++ FinalServer/Main.java
@@ -4,19 +4,17 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Main {
-  public static void main(String[] args) {
-    try(ServerSocket serverSocket = new ServerSocket(6000)) {
-        while(true) {
-            new Server(serverSocket.accept()).start();
-//                Socket socket = serverSocket.accept(); //accept
-//                Echoer echoer = new Echoer(socket);
-//                echoer.start();
-        }
 
-    } catch(IOException e) {
-        System.out.println("Server exception " + e.getMessage());
+  public static void main(String[] args) {
+    try (ServerSocket serverSocket = new ServerSocket(6000)) {
+      while (true) {
+        new Server(serverSocket.accept()).start();
+        //                Socket socket = serverSocket.accept(); //accept
+        //                Echoer echoer = new Echoer(socket);
+        //                echoer.start();
+      }
+    } catch (IOException e) {
+      System.out.println("Server exception " + e.getMessage());
     }
-}
-  
-  
+  }
 }
