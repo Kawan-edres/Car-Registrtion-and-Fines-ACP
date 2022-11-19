de88dfe78fc13c09ea419f34b5e494ba653c8fce
Naly Moslih <95342461+Nalymoslih@users.noreply.github.com>
2022-11-20 02:18:42 +0300

    Update Client2.java

--- FinalServer/Client2.java
+++ FinalServer/Client2.java
@@ -13,3 +13,14 @@ public static void main(String[] args) throws IOException {
         Socket echoSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
+        
+        try {
+            echoSocket = new Socket("localhost", 6000);
+            System.out.println("connected to: "+echoSocket);
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
