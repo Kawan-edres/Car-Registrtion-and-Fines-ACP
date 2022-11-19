4bad66adc325510aea76d13a7fbe264c58e46dea
Bahez <95364328+Bahezz@users.noreply.github.com>
2022-11-20 02:33:00 +0300

    Update Main.java
    Adding the Main class that handles the multithreading

--- FinalServer/Main.java
+++ FinalServer/Main.java
@@ -1,9 +1,21 @@
 package FinalServer;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main {
-public static void main(String[] args) {
-  Server server=new Server();
-  server.startConnection();
+  public static void main(String[] args) {
+    try(ServerSocket serverSocket = new ServerSocket(6000)) {
+        while(true) {
+            new Server(serverSocket.accept()).start();
+//                Socket socket = serverSocket.accept(); //accept
+//                Echoer echoer = new Echoer(socket);
+//                echoer.start();
+        }
+
+    } catch(IOException e) {
+        System.out.println("Server exception " + e.getMessage());
+    }
 }
   
   
